59a6d238cdff84cb7a9440fe0f78db7503f7349b
dae-ne <rafalcz29@gmail.com>
2025-11-14 17:03:49 +0100

    SetSubtaskLabels code improvements

--- integrations/Todoist/src/Functions/SetSubtaskLabels.cs
+++ integrations/Todoist/src/Functions/SetSubtaskLabels.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
@@ -11,15 +12,19 @@ internal sealed class SetSubtaskLabels(ITodoistApi todoist, ILogger<SetSubtaskLa
 {
     [Function(nameof(SetSubtaskLabels))]
     public async Task RunAsync(
-        [TimerTrigger("0 0 2 * * *", UseMonitor = false, RunOnStartup = true)] TimerInfo timer)
+        [TimerTrigger("0 0 2 * * *", UseMonitor = false, RunOnStartup = true)] TimerInfo _)
     {
         logger.LogInformation("Timer trigger function executed at: {TriggerTime}", DateTime.Now);
 
-        await HandleFunctionAsync();
-
-        if (timer.ScheduleStatus is null) return;
-
-        logger.LogInformation("Next timer schedule at: {NextTriggerAt}", timer.ScheduleStatus.Next);
+        try
+        {
+            await HandleFunctionAsync();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error while setting subtask labels.");
+            throw;
+        }
     }
 
     private async Task HandleFunctionAsync()
@@ -27,32 +32,63 @@ internal sealed class SetSubtaskLabels(ITodoistApi todoist, ILogger<SetSubtaskLa
         const string subtaskLabel = "subtask";
 
         var tasks = (await GetTasksAsync())
-            .Where(t => !string.IsNullOrWhiteSpace(t.ParentId)); // just to make sure
+            .Where(t => !string.IsNullOrWhiteSpace(t.ParentId)) // just to make sure
+            .Where(t => !t.Labels.Contains(subtaskLabel) || t.Labels.Count() > 1)
+            .ToArray();
 
-        var updateRequest = new TodoistUpdateTaskRequest
+        if (tasks.Length == 0)
         {
-            Labels = [subtaskLabel]
-        };
-
-        foreach (var task in tasks)
-        {
-            await todoist.UpdateTaskAsync(task.Id, updateRequest);
+            logger.LogInformation("No subtasks to update.");
+            return;
         }
+
+        var updatedCount = await UpdateSubtaskLabelsAsync(tasks, subtaskLabel);
+
+        logger.LogInformation("Updated labels for {UpdatedCount} subtasks.", updatedCount);
     }
 
     private async Task<IEnumerable<TodoistTask>> GetTasksAsync()
     {
-        const string filter = "subtask";
+        const string requestQuery = "subtask";
 
         TodoistResponse? response = null;
         List<TodoistTask> tasks = [];
 
         do
         {
-            response = await todoist.GetTasksByFilterAsync(query: filter, cursor: response?.NextCursor);
+            response = await todoist.GetTasksByFilterAsync(query: requestQuery, cursor: response?.NextCursor);
             tasks.AddRange(response.Results);
         } while (!string.IsNullOrWhiteSpace(response.NextCursor));
 
         return tasks;
+    }
+
+    private async Task<int> UpdateSubtaskLabelsAsync(IEnumerable<TodoistTask> tasks, string subtaskLabel)
+    {
+        var semaphoreSlim = new SemaphoreSlim(5);
+        var updateCounter = 0;
+
+        var updateRequest = new TodoistUpdateTaskRequest
+        {
+            Labels = [subtaskLabel]
+        };
+
+        var apiCallTasks = tasks.Select(async task =>
+        {
+            await semaphoreSlim.WaitAsync();
+
+            try
+            {
+                await todoist.UpdateTaskAsync(task.Id, updateRequest);
+                Interlocked.Increment(ref updateCounter);
+            }
+            finally
+            {
+                semaphoreSlim.Release();
+            }
+        });
+
+        await Task.WhenAll(apiCallTasks);
+        return updateCounter;
     }
 }
